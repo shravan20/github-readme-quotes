30876edae52675faf82dd3d44f806e7453052a18
shravankb <shravan.kumar@nineleaps.com>
2020-11-17 01:23:50 +0530

    [ Folder Structure ] feature #001 Basic setup done

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3002;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
