0fc30e1575e0d9212c4391a7f15a4d32eb87f7c3
nishantpersonal <nishantvenugopalnair@gmail.com>
2020-11-29 09:07:36 +0530

    [Quote Service] refactor Added services for quotes generator

--- src/api/controllers/quotesController.js
+++ src/api/controllers/quotesController.js
@@ -1,40 +1,23 @@
 const themes = require("../../themes/themes");
-const cardTemplate = require('../../utils/generateTemplate');
-const requestApi =require('../../utils/fetchApi');
-const url = process.env.SERVICE_URL;
 const animations = require("../../animations/animation");
-const Template=require("../../models/Template");
+const layouts=require("../../layouts/layout");
+const quoteService=require("../services/quotesService");
 
-const getQuote = async (req, res, next) => {
+const quoteController = async (req, res, next) => {
   
   try {
+     let theme = themes[req.query.theme] ? themes[req.query.theme] : themes["default"];
+
+     let animation = animations[req.query.animation] ? animations[req.query.animation] 
+                    : animations["default"];
     
-    let apiResponse = await requestApi(url);
+     let layout=layouts[req.query.layout] ? layouts[req.query.layout] 
+                    : layouts["default"];
 
-    let theme = themes[req.query.theme ? req.query.theme : "default"];
-    theme = theme ? theme: themes["default"];
+     let reqObj={theme,animation,layout}
 
-    let animation = animations[req.query.animation ? req.query.animation : "default"];
-    
-    let layout=req.query.layout;
+    await quoteService.getQuote(reqObj,res);
 
-    const template=new Template();
-    template.setTheme(theme);
-    template.setData(apiResponse);
-    template.setAnimation(animation);
-    template.setLayout(layout);
-
-    let svg = cardTemplate.generateTemplate(template);
-    
-    res.setHeader("Content-Type", "image/svg+xml");
-    res.header(
-      "Cache-Control",
-      "no-cache,max-age=0,no-store,s-maxage=0,proxy-revalidate"
-    );
-    res.header("Pragma", "no-cache");
-    res.header("Expires", "-1");
-    res.send(svg);
-  
   } catch (error) {
     
     res.send({
@@ -46,5 +29,5 @@ const getQuote = async (req, res, next) => {
 };
 
 module.exports = {
-  getQuote
+  quoteController
 };
