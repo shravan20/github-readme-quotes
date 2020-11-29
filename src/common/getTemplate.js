c8118a7a1c2e3e26d367230b6c9caf9c0cf7fd64
nishantpersonal <nishantvenugopalnair@gmail.com>
2020-11-29 17:18:01 +0530

    Svg height refactor

--- src/common/getTemplate.js
+++ src/common/getTemplate.js
@@ -1,6 +1,6 @@
 const getTemplate=template=>{
     return `
-    <svg width="700px" height="250px" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="700px" height="300px" fill="none" xmlns="http://www.w3.org/2000/svg">
     <foreignObject width="100%" height="100%">
         <div xmlns="http://www.w3.org/1999/xhtml">
             <style>
