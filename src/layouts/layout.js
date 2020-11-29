161ef76a820cb8292ba72d810c1ab459507593db
nishantpersonal <nishantvenugopalnair@gmail.com>
2020-11-29 11:59:40 +0530

    [layout] feature Added churchill layout

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -106,6 +106,79 @@ const layouts = {
                     </div>`;
     },
   },
+  churchill: {
+    style: (template) => {
+      return `#ct{
+                  height:auto 
+                  width:100px;
+                  border:1px solid #f1c40f;
+                  margin: 20px 50px 20px 10px;
+                  text-align:center;
+                  position:relative;
+                  color:#${template.theme.quote_color};
+                  padding:15px;
+                  background:#${template.theme.bg_color};
+                }
+                span{
+                    background:#${template.theme.bg_color};
+                  color:#${template.theme.author_color};
+                  padding:0 10px;
+                  font-size:20px;
+                  position:relative; 
+                  top:-28px;
+                }
+                .corner{
+                  height:30px;
+                  width:30px;
+                  border-radius:50%;
+                  border:1px solid #fff;
+                  transform:rotate(-45deg);  
+                  position:absolute;
+                  background:#fff;
+                }
+
+                #left_top{
+                  top:-16px;
+                  left:-16px;
+                  border-color:transparent transparent #f1c40f transparent;
+                }
+                #right_top{
+                  top:-16px;
+                  right:-16px;
+                  border-color:transparent transparent transparent #f1c40f;
+                }
+                #left_bottom{
+                  bottom:-16px;
+                  left:-16px;
+                  border-color:transparent #f1c40f transparent transparent ;
+                }
+                #right_bottom{
+                  bottom:-16px;
+                  right:-16px;
+                  border-color:#f1c40f transparent transparent transparent;
+                }
+                p{
+                  padding-top:0px;
+                  font-size:18px
+                }`;
+    },
+    structure: (template) => {
+      return `<div id="ct">
+                  <div class="corner" id="left_top"></div>
+                  <div class="corner" id="left_bottom"></div>
+                  <div class="corner" id="right_top"></div>
+                  <div class="corner" id="right_bottom"></div>
+                  <span>${
+                    template.author === "Unknown"
+                      ? "Anonymous"
+                      : template.author
+                  }</span>
+                  <blockquote>
+                    <p><i>${template.quote}</i></p>
+                  </blockquote>
+                </div>`;
+    },
+  },
 };
 
 module.exports = layouts;
