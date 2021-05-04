94a85884f151efda6825b01f942520c5121c6ade
olushesitoheeb <olaolushesi98@gmail.com>
2021-05-04 13:09:49 +0100

    move svgs to assets and added product hunt button

--- frontend/src/util/styles/componentStyles.js
+++ frontend/src/util/styles/componentStyles.js
@@ -1,13 +1,16 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core';
 
 export const getAppBarStyles = makeStyles((theme) => ({
     title: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
-    themeIcon:{
-        backgroundColor:"transparent !important",
-        height:"38px",
-        width:"38px",
-        boxShadow:"0px 0px 0.5px 0.5px #6e7681"
-    }
-  }));
+    themeIcon: {
+        backgroundColor: 'transparent !important',
+        height: '38px',
+        width: '38px',
+        boxShadow: '0px 0px 0.5px 0.5px #6e7681',
+    },
+    productHuntBtn: {
+        flexGrow: 1,
+    },
+}));
