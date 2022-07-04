3263c878ab56f6b340c82133beec78f2c67bef77
Vikram G <vikram.ghangurde98@gmail.com>
2022-07-03 21:33:20 -0700

    Added swagger doc

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ const initiateServer = () => {
 
     const app = express();
 
+    // Serve SwaggerUi docs
+    const swaggerUi = require('swagger-ui-express');
+    const swaggerDocument = require('./swagger.json');
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }))
 
