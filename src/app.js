2ae9c1dcccce063ef8f8bbbca5e028a8a2b8d77d
Vikram G <vikram.ghangurde98@gmail.com>
2022-07-03 22:40:35 -0700

    added swagger docs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const routes = require('./api/routes/quotes-router');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require("path");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const initiateServer = () => {
 
@@ -24,6 +26,34 @@ const initiateServer = () => {
     app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../","frontend", "/", "build", "index.html"));
     });
+
+    const options = {
+      definition: {
+        openapi: "3.0.0",
+        info: {
+          title: "GitHub Readme Quotes",
+          version: "0.1.0",
+          description:
+            "Dynamic quote generator for your GitHub readmes | Give a poetic touch to readmes",
+          license: {
+            name: "MIT"
+          }
+        },
+        servers: [
+          {
+            url: "https://github-readme-quotes.herokuap.com/v1",
+          },
+        ],
+      },
+      apis: ["./src/api/routes/quotes-router.js"],
+    };
+
+    const specs = swaggerJsdoc(options);
+    app.use(
+      "/api-docs",
+      swaggerUi.serve,
+      swaggerUi.setup(specs)
+    );
     app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
 
 }
