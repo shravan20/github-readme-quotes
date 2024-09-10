04a4ae70732bbfe6bc442045036a50f46db1e684
Siddharth <siddharth122002@gmail.com>
2024-09-10 23:39:08 +0530

    fixed/navbar

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -14,7 +14,8 @@ import MoonIcon from '../../assets/moon.svg';
 import LightIcon from '../../assets/sun.svg';
 import Home from '../Pages/Home';
 import DiscordButton from './discord';
-
+import styleNav from '../../styles/styleNav.css'
+import { FaProductHunt } from "react-icons/fa";
 const ElevationScroll = (props) => {
     const { children, window } = props;
     // Note that you normally won't need to set the window ref as useScrollTrigger
@@ -53,27 +54,30 @@ const Dashboard = (props) => {
                                     variant='h6'
                                     className={classes.title}
                                 >
-                                    {dashConstants.APP_NAME}
+                                    <div className='text'>
+                                        {dashConstants.APP_NAME}
+                                    </div>
                                 </Typography>
-                                <DiscordButton />
-                                <IconButton>
-                                    <a
-                                        href='https://www.producthunt.com/posts/dynamic-github-profile-readme-quotes?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-dynamic-github-profile-readme-quotes'
-                                        target='_blank'
-                                        rel='noreferrer'
-                                    >
-                                        <img
-                                            src='https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=276934&theme=light'
-                                            alt='Dynamic GitHub Profile Readme Quotes - Everlasting Poetic Touch to GitHub Profiles for everyone | Product Hunt'
-                                            style={{
-                                                width: ' 250px',
-                                                height: '54px',
-                                            }}
-                                            width='250'
-                                            height='54'
-                                        />
-                                    </a>
-                                </IconButton>
+                                <div className='center'>
+                                    <DiscordButton />
+                                    
+                                        <a className='p'
+                                            href='https://www.producthunt.com/posts/dynamic-github-profile-readme-quotes?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-dynamic-github-profile-readme-quotes'
+                                            target='_blank'
+                                            rel='noreferrer'
+                                        >
+                                            <FaProductHunt className='product-sm'/>
+                                            <img
+                                                className='product'
+                                                src='https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=276934&theme=light'
+                                                alt='Dynamic GitHub Profile Readme Quotes - Everlasting Poetic Touch to GitHub Profiles for everyone | Product Hunt'
+                                               
+                                            />
+
+                                        </a>
+                                   
+
+                                </div>
                                 <IconButton
                                     className={classes.themeIcon}
                                     onClick={handleThemeChange}
